d6302c7adc3734f61d683e38589b771fd35b91e2
TobleMiner <tobleminer@gmail.com>
2014-01-14 13:08:36 +0100

    Now really deleted unused test classes

--- src/TobleMiner/MineFight/Air/Missiles/Tomahawk/TomahawkMode.java
+++
@@ -1,9 +0,0 @@
-package TobleMiner.MineFight.Air.Missiles.Tomahawk;
-
-public enum TomahawkMode
-{
-	GroundProfile,
-	HeightProfile,
-	DrAtmosphereProfile, //In the rainbow factory where your fears and horrors come true
-	HighSpeedProfile
-}
