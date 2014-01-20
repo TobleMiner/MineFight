d7302382a0faa354d3dc64f67d337335272f2eb8
TobleMiner <tobleminer@gmail.com>
2014-01-20 21:25:17 +0100

    Added Multiverse handler Till now it's just an pretty empty stub, but I'll grt it working soon.

---
+++ src/TobleMiner/MineFight/Multiverse/MultiverseHandler.java
@@ -0,0 +1,26 @@
+package TobleMiner.MineFight.Multiverse;
+
+import org.bukkit.plugin.Plugin;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MultiversePlugin;
+
+import TobleMiner.MineFight.ErrorHandling.Error;
+import TobleMiner.MineFight.ErrorHandling.ErrorReporter;
+import TobleMiner.MineFight.ErrorHandling.ErrorSeverity;
+
+public class MultiverseHandler 
+{
+
+	private final MultiverseCore mc;
+	
+	public MultiverseHandler(Plugin p) 
+	{
+		if(!(p instanceof MultiversePlugin))
+		{
+			Error err = new Error("Type of Multiverse isn't MultiversePlugin", "Something about your Multiverse seams to be wrong.", "Make sure you have the latest version of Multiverse installed!", this.getClass().getName(), ErrorSeverity.WARNING);
+			ErrorReporter.reportError(err);
+		}
+		this.mc = ((MultiversePlugin)p).getCore();
+	}
+}
---
+++ src/TobleMiner/MineFight/Multiverse/MultiverseLoader.java
@@ -0,0 +1,14 @@
+package TobleMiner.MineFight.Multiverse;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+public class MultiverseLoader
+{
+	public MultiverseHandler getHandler()
+	{
+		Plugin p = Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+		if(p == null) return null;
+		return new MultiverseHandler(p);
+	}
+}
